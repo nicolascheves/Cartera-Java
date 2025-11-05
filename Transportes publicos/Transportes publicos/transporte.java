b7a645fc88f1928142b15aa43282a348b14c17f8
nicolascheves <111844191+nicolascheves@users.noreply.github.com>
2025-11-05 08:54:42 -0300

    Delete Transportes publicos/Transportes publicos/transporte.java

--- Transportes publicos/Transportes publicos/transporte.java
+++
@@ -1,6 +0,0 @@
-public abstract class transporte {
-    abstract int getCapacidad();
-    abstract double getKilometrosRecorridos();
-    abstract double getCostoMantenimiento();
-    abstract int getModelo();
-}
