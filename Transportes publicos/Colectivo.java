8717725ea7332d8943d0cbc21ede98e2b67b3cba
nicolascheves <111844191+nicolascheves@users.noreply.github.com>
2025-11-05 09:40:40 -0300

    Colectivo urbno & med distc
    Aqui representamos una clase que la puede adoptar tanto un colectivo urbano como un colectivo media distancia, cada uno tiene un costo distinto pero la formula para su mantenimiento, getCostoMentenimiento(), es la misma para ambos no hace falta otra clase para el mismo metodo, this.costo_por * this.getAsientos()

--- Transportes publicos/Colectivo.java
+++ Transportes publicos/Colectivo.java
@@ -1,5 +1,21 @@
-public class Colectivo exteds TransportePublico{
-    public Colectivo(String patente, int anios_modelo, int asientos){
-        super(patente, anios_modelo, asientos);
+import parcial.Vehiculo;
+
+abstract public class Colectivo extends Vehiculo{
+    private double costo_por;
+
+    public Colectivo(double costo_por){
+        // falta super(con parametros del padre, para una construccion completa)
+        this.costo_por = costo_por;
     }
+
+    public double getMantenimiento(){
+        return this.costo_por * this.getAsientos() ;
+    } 
 }
+
+// revisar atributos de c/ transporte
+// this. esta bien en return? 
+
+// lo mismo p/ colectivo corta media dist
+// mant diario
+// se definen atributos de vehiculos por un lado y los de taxi x otro?
