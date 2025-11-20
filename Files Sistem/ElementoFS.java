f8a86ab5acb9cb636c5c5bd211b8896719e1678c
nicolascheves <111844191+nicolascheves@users.noreply.github.com>
2025-11-20 20:38:06 -0300

    Add metodos trabajados
    Agrego metodo de busqueda con criterio a profundiad con tipos de retornos variados. Agrego metodo de copia con criterio y simple a profundidad.

--- Files Sistem/ElementoFS.java
+++ Files Sistem/ElementoFS.java
@@ -1,4 +1,11 @@
 public abstract class ElementoFS {
+    public abstract int getCantidad();
+    public abstract double getTamanio();
+    public abstract int archivosDeInteres(Condicion cc);
+    public abstract ArrayList<ElementoFS> buscar(Condicion cc);
+    public abstract Archivo getSiguiente(Comparator<Archivo> estrategia);
     public abstract ElementoFS getCopia(Condicion cc);
     public abstract ElementoFS getCopia();
+    public abstract ElmentoFS duplicar();
 }
+
