33316ca85adf70bf5d81414fb1e7c1f3e22324e5
nicolascheves <111844191+nicolascheves@users.noreply.github.com>
2025-11-21 15:48:43 -0300

    Agrg getSiguiente especifico en Archivo
    getSiguiente usa (Comparetor estrategia) aqui no lo usa ya que se habia pedido un valor booleano del objeto para considerarlo, como no es el caso de Archivo se uso el tamanio del como filtro.

--- Files Sistem/Archivo.java
+++ Files Sistem/Archivo.java
@@ -1,10 +1,16 @@
 public class Archivo {
     protected String name;
 
-    // public asbtract ElementoFS getCopia(Condicion cc);
-
     public Archivo(String name){
         this.name = name;
+    }
+
+    // getSiguiente ~= buscar
+    // no usa estrategia aca, sino UNA SI DEFINIDA (this.getEntregado() Booleano)
+    public ElementoFS getSiguiente(Comparator<Archivo> estrategia){
+        if(this.getTamanio()> 0.5){
+            return this;
+        } else return null;
     }
     
     // copia indiscriminada de Archivos
@@ -25,4 +31,5 @@ public class Archivo {
         }
     }
 }
+
 
